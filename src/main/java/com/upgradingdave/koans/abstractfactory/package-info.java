58d43aad3fcb9ff5889b3a76ee1977a471d05ab6
Dave Paroulek <dparoulek@gmail.com>
2011-01-06 15:26:53 -0500

    added documentation

---
+++ src/main/java/com/upgradingdave/koans/abstractfactory/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Example of the Abstract Factory Pattern from 
+ * "Design Patterns: Elements of Reusable Object-Oriented Software"
+ * 
+ * &copy; 2011 Dave Paroulek2008
+ */
+package com.upgradingdave.koans.abstractfactory;
